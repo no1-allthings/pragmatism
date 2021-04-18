0e234bae55d98ccbf22e7f884ac09d4a6d90aab8
DROP RAIN <no1.allthings@gmail.com>
2021-04-18 18:53:49 +0900

    post-save

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import Hello
+
 
 def hello(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello.html', context={'te': 'post!!'})
+        temp = request.POST.get('hello_input')
+        new_hello = Hello()
+        new_hello.text = temp
+        new_hello.save()
+        return render(request, 'accountapp/hello.html', context={'hello_output': new_hello})
     else:
-        return render(request, 'accountapp/hello.html', context={'te': 'GET!!'})
+        return render(request, 'accountapp/hello.html', context={'hello_output': 'GET!!'})
